4c3d7970bfb21461dc8a6454e9c365de78c65bae
madport <portanova2002@gmail.com>
2022-02-18 22:02:42 +0100

    HandTracking class working
    Signed-off-by: madport <portanova2002@gmail.com>

--- src/handTracking/basicCamera.py
+++ src/handTracking/basicCamera.py
@@ -1,42 +1,36 @@
 import cv2
-import mediapipe as mp
-import time
+import mediapipe
+ 
+drawingModule = mediapipe.solutions.drawing_utils
+handsModule = mediapipe.solutions.hands
+ 
+capture = cv2.VideoCapture(0)
+ 
+frameWidth = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+frameHeight = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+ 
+ 
+with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=1) as hands:
+ 
+    while (True):
+ 
+        ret, frame = capture.read()
+ 
+        results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+ 
+        if results.multi_hand_landmarks != None:
+            handLandmarks = results.multi_hand_landmarks
 
-cap = cv2.VideoCapture(0)
-
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=2,
-                      min_detection_confidence=0.5,
-                      min_tracking_confidence=0.5)
-mpDraw = mp.solutions.drawing_utils
-
-pTime = 0
-cTime = 0
-
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x *w), int(lm.y*h)
-                #if id ==0:
-                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
-
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
-
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-
-    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+            normalizedLandmark = handLandmarks[0].landmark[4]
+            pixelCoordinatesLandmark = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, frameWidth, frameHeight)
+            print(handLandmarks[0].landmark[4].x, " - ", pixelCoordinatesLandmark, "\n")
+            cv2.circle(frame, pixelCoordinatesLandmark, 5, (0, 255, 0), -1)
+ 
+ 
+        cv2.imshow('Test hand', frame)
+ 
+        if cv2.waitKey(1) == 27:
+            break
+ 
+cv2.destroyAllWindows()
+capture.release()
